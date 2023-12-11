669cfdb76fcb3a6dacb006437377825b2167e207
Miguel Olave <molavec@gmail.com>
2023-12-11 17:55:49 -0300

    Fix: error router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 // App.tsx
-import React, { useEffect, useState } from 'react';
-import TopBar from './components/TopBar.tsx';
-import HorizontalLinearStepper from './components/HorizontalLinearStepper.tsx';
-import Footer from './components/Footer.tsx';
+import { useEffect, useState } from 'react';
+// import TopBar from './components/TopBar.tsx';
+// import HorizontalLinearStepper from './components/HorizontalLinearStepper.tsx';
+// import Footer from './components/Footer.tsx';
 
 // import AppRoutes from './Routes';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from '../src/pages/login/AuthContext';
 import LoginScreen from './pages/login/LoginScreen.tsx';
 //import { PrivateRoute } from './auth/PrivateRoute';
 import SideBar from './components/SideBar.tsx';
-import UsoInternoFinanzas from './pages/usointfinanzas/UsointernoFinanzasForm';
-import UsoInternoDGEC from './pages/usointdgec/UsointernoDGEC';
-import UsointernoDireccionEstudios from './pages/usointdireccionestudios/UsointernoDireccionEstudios';
+// import UsoInternoFinanzas from './pages/usointfinanzas/UsointernoFinanzasForm';
+// import UsoInternoDGEC from './pages/usointdgec/UsointernoDGEC';
+// import UsointernoDireccionEstudios from './pages/usointdireccionestudios/UsointernoDireccionEstudios';
 import Formulario from './components/Form.tsx';
 import { setupAxiosInterceptors } from './pages/login/axiosConfig.ts';
 
 function App() {
   const [ isAuthenticated, setIsAuthenticated ] = useState(false);
   
-  const handleLogin = (token:any) => {
+  const handleLogin = (token: string) => {
+    console.log(token)
     setIsAuthenticated(true);
     //Store token securely (e.g., in-memory)
   };
@@ -33,20 +34,23 @@ function App() {
   }, []);
 
     return (
-      
         <AuthProvider login={handleLogin} logout={handleLogout}>
           {isAuthenticated && <SideBar />}
-        <Routes>
-          <Route path="/" element={<LoginScreen />} />
-          <Route path="/home" element={<SideBar/>} />
-          <Route path="/formulario" element={<Formulario/>} />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LoginScreen />} />
+              <Route path="/home" element={<SideBar/>} />
+              <Route path="/formulario" element={<Formulario/>} />
 
-          <Route path="/finanzas" element={<UsoInternoFinanzas />} />
-          <Route path="/Dgec" element={<UsoInternoDGEC />} />
-          <Route path="/DireccionEstudios" element={<UsointernoDireccionEstudios/>} /> 
-       </Routes>
-       </AuthProvider>   
-      
+              {/* A JSX comment 
+              <Route path="/finanzas" element={<UsoInternoFinanzas />} />
+              <Route path="/Dgec" element={<UsoInternoDGEC />} />
+              <Route path="/DireccionEstudios" element={<UsointernoDireccionEstudios/>} /> 
+              */} 
+              
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>   
     );
   };
 
