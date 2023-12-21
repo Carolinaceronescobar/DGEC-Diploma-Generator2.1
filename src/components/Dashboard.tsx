33ef423e8d0811226e3770197e6eb8408b414f06
Miguel Olave <molavec@gmail.com>
2023-12-21 16:26:12 -0300

    Corrección CSS del Header y se elimina el sidebar

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 //DGEC-DIPLOMA-GENERATOR2/ui/components/Dashboard.tsx
-import React, { useState } from 'react';
+import React from 'react';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import AddIcon from '@mui/icons-material/Add';
@@ -109,110 +109,49 @@ const Dashboard: React.FC = () => {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-
-        <HeaderApp />
-
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-
-          <List component="nav">
-            {mainListItemsFx().map((item) => (
-              <ListItem
-                key={item.id}
-                button
-                onClick={() => navigate(item.route)}
-              >
-                {/* Tu contenido individual del elemento de la lista */}
-                <ListItemText primary={item.text} />
-                {/* ... otros componentes de elementos de lista según sea necesario */}
-              </ListItem>
-            ))}
-          </List>
-
-          <Divider sx={{ my: 1 }} />
-
-          <List component="nav">
-            {secondaryListItems.map((item) => (
-              <ListItem key={item.id}>
-                {/* Tu contenido individual del elemento de la lista */}
-                <ListItemText primary={item.text} />
-                {/* ... otros componentes de elementos de lista según sea necesario */}
-              </ListItem>
-            ))}
-
-            <ListItem>
-              <ModalComponent />
-            </ListItem>
-          </List>
-        </Drawer>
-
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-              <Grid
-                container
-                direction="row-reverse"
-                justifyContent="flex-start"
-                spacing={2}
-              >
-                <Grid item>
-                  <Paper className="p-2" sx={{ margin: '25px' }}>
-                    <Link
-                      to="/formulario"
-                      style={{
-                        textDecoration: 'none',
-                      }}
+      <CssBaseline />
+      <HeaderApp />
+      <Box sx={{ display: 'flex', marginTop: '90px' }}>
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              container
+              direction="row-reverse"
+              justifyContent="flex-start"
+              spacing={2}
+            >
+              <Grid item>
+                <Paper className="p-2" sx={{ margin: '25px' }}>
+                  <Link
+                    to="/formulario"
+                    style={{
+                      textDecoration: 'none',
+                    }}
+                  >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      startIcon={<AddIcon />}
+                      sx={{ backgroundColor: '#004B85' }}
                     >
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        startIcon={<AddIcon />}
-                        sx={{ backgroundColor: '#004B85' }}
-                      >
-                        Nuevo formulario
-                      </Button>
-                    </Link>
-                  </Paper>
-                </Grid>
+                      Nuevo formulario
+                    </Button>
+                  </Link>
+                </Paper>
               </Grid>
-              <Grid container spacing={3}>
-                {/* primer  row */}
-                <Grid item xs={12} style={{ marginTop: '3vh' }}>
-                  {/* Integra SolicitudesTabla con los datos */}
-                  {/* <SolicitudesTabla solicitudes={solicitudesData} /> */}
-                </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              {/* primer  row */}
+              <Grid item xs={12} style={{ marginTop: '3vh' }}>
+                {/* Integra SolicitudesTabla con los datos */}
+                {/* <SolicitudesTabla solicitudes={solicitudesData} /> */}
               </Grid>
-            </Paper>
-            <Footer />
-          </Container>
-        </Box>
+            </Grid>
+          </Paper>
+        </Container>
       </Box>
+
+      <Footer />
     </ThemeProvider>
   );
 };
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   Drawer,
   Toolbar,
@@ -9,61 +8,51 @@ import {
   ListItemText,
 } from '@mui/material';
 import { ChevronLeft as ChevronLeftIcon } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
-import { mainListItems } from './../pages/resumen/listItems';
 
-interface SidebarProps {
-  secondaryListItems: { id: number; text: string; route: string }[];
-  toggleDrawer: () => void;
-  open: boolean;
-}
 // Crear un nuevo componente para la barra lateral
-const Sidebar: React.FC<SidebarProps> = ({
-  secondaryListItems,
-  toggleDrawer,
-  open,
-}) => {
-  const navigate = useNavigate();
-  const mainListItemsFx = () => mainListItems;
-
+const Sidebar = () => {
   return (
-    <>
-      <Drawer variant="permanent" open={open}>
-        <Toolbar
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            px: [1],
-          }}
-        >
-          <IconButton onClick={toggleDrawer}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </Toolbar>
-        <Divider />
+    <Drawer variant="permanent" open={open}>
+      <Toolbar
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          px: [1],
+        }}
+      >
+        <IconButton onClick={toggleDrawer}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </Toolbar>
+      <Divider />
 
-        <List component="nav">
-          {/* Lista de elementos en la barra lateral */}
-          {mainListItemsFx().map((item) => (
-            <ListItem key={item.id} button onClick={() => navigate(item.route)}>
-              <ListItemText primary={item.text} />
-            </ListItem>
-          ))}
-        </List>
+      <List component="nav">
+        {mainListItemsFx().map((item) => (
+          <ListItem key={item.id} button onClick={() => navigate(item.route)}>
+            {/* Tu contenido individual del elemento de la lista */}
+            <ListItemText primary={item.text} />
+            {/* ... otros componentes de elementos de lista según sea necesario */}
+          </ListItem>
+        ))}
+      </List>
 
-        <Divider sx={{ my: 1 }} />
+      <Divider sx={{ my: 1 }} />
 
-        <List component="nav">
-          {/* Otros elementos de la barra lateral, como el ModalComponent */}
-          {secondaryListItems.map((item) => (
-            <ListItem key={item.id} button onClick={() => navigate(item.route)}>
-              <ListItemText primary={item.text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-    </>
+      <List component="nav">
+        {secondaryListItems.map((item) => (
+          <ListItem key={item.id}>
+            {/* Tu contenido individual del elemento de la lista */}
+            <ListItemText primary={item.text} />
+            {/* ... otros componentes de elementos de lista según sea necesario */}
+          </ListItem>
+        ))}
+
+        <ListItem>
+          <ModalComponent />
+        </ListItem>
+      </List>
+    </Drawer>
   );
 };
 
