31ae40df97be7711e23daf454e9179387933b9d4
Miguel Olave <molavec@gmail.com>
2023-12-21 15:53:20 -0300

    HeaderApp simplificado funcionando.

--- src/components/HeaderApp.tsx
+++ src/components/HeaderApp.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   Badge,
+  Box,
   IconButton,
   Menu,
   MenuItem,
@@ -9,38 +10,10 @@ import {
   Typography,
 } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import { styled } from '@mui/material/styles';
-
-// interface HeaderProps {}
-
-const drawerWidth = 240;
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+import AppBar from '@mui/material/AppBar';
 
 export const HeaderApp = () => {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-  const [open, setOpen] = React.useState(true);
   const navigate = useNavigate();
 
   const handleClose = () => {
@@ -64,34 +37,40 @@ export const HeaderApp = () => {
   };
 
   return (
-    <AppBar position="absolute" open={open} sx={{ backgroundColor: '#004B85' }}>
-      <Toolbar
-        sx={{
-          pr: '24px', // keep right padding when drawer closed
-        }}
-      >
+    <AppBar
+      position="fixed"
+      sx={{
+        backgroundColor: '#004B85',
+      }}
+    >
+      <Toolbar>
         {/* Reemplaza NotificationsIcon con AccountCircleIcon o el icono de perfil que prefieras */}
 
         {/*Logotipo de la Universidad */}
-        <img
-          alt="logo"
-          src="./src/assets/logo-usm_blanco-min.png"
-          style={{
-            width: 150,
-            height: 25,
-            marginTop: 30,
-            marginLeft: 30,
-          }}
-        />
-        <Typography
-          component="h1"
-          variant="h6"
-          color="inherit"
-          noWrap
-          sx={{ flexGrow: 1 }}
-        >
-          Direccion General de Estudios Continuos
-        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <div>
+            <img
+              alt="logo"
+              src="./src/assets/logo-usm_blanco-min.png"
+              style={{
+                width: 150,
+                height: 25,
+                marginTop: 30,
+                marginLeft: 30,
+              }}
+            />
+          </div>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Direccion General de Estudios Continuos
+          </Typography>
+        </Box>
+
         <IconButton color="inherit" onClick={handleOpenProfileMenu}>
           <Badge color="secondary">
             <AccountCircleIcon />
