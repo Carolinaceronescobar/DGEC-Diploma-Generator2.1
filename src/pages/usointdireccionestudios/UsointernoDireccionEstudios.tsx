02c9f1e78787ac35be499b6fd111e5c8f102afa9
Carolinaceronescobar <kro.ceron@gmail.com>
2023-12-26 03:36:14 -0300

    direst arreglo caja de edicion/eliminar y añadir nuevo

--- src/pages/usointdireccionestudios/UsointernoDireccionEstudios.tsx
+++ src/pages/usointdireccionestudios/UsointernoDireccionEstudios.tsx
@@ -8,6 +8,14 @@ import {
   Button,
   Box,
   Container,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Grid,
+  TextField,
 } from '@mui/material';
 
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
@@ -86,6 +94,65 @@ const UsoInternoDireccionEstudios: React.FC<
   onGuardar,
   onEnviar,
 }) => {
+  const [selectedModule, setSelectedModule] = useState('');
+  const [tableData, setTableData] = useState<string[]>([]);
+  const [optionsCodedgec, setOptionsCodedgec] = useState([
+    'Codigo Int DGEC1',
+    'Codigo Int DGEC2',
+    'Codigo Int DGEC3',
+  ]);
+
+  const [optionsDirest, setOptionsDirest] = useState([
+    'DIREST2023-1',
+    'DIREST2022-2',
+    'DIREST2024-1',
+  ]);
+
+  const handleDelete = (index: number) => {
+    const updatedTableData = [...tableData];
+    updatedTableData.splice(index, 1);
+    setTableData(updatedTableData);
+  };
+
+  const handleEdit = (index: any) => {
+    setinputAutocompleteModuleValue(tableData[index]);
+    handleDelete(index);
+  };
+
+  const [inputAutocomplete, setinputAutocompleteModuleValue] = useState('');
+
+  const handleAdd = () => {
+    let val = inputAutocomplete;
+    let tabla_temporal = [...tableData, inputAutocomplete];
+    setTableData(tabla_temporal);
+  };
+
+  const handleInputAutoCompleteChange = (event: any, value_input: any) => {
+    setinputAutocompleteModuleValue(value_input);
+  };
+
+  const handleInputDirestChange = (event: any) => {
+    const newValue = event.target.value;
+    setinputAutocompleteModuleValue(newValue);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (
+      event.key === 'Enter' &&
+      inputAutocomplete.trim() !== '' &&
+      optionsCodedgec.findIndex((z) => z == inputAutocomplete) == -1
+    ) {
+      agregarNuevoValor(inputAutocomplete);
+    }
+  };
+
+  const agregarNuevoValor = (valor: string) => {
+    if (optionsCodedgec.findIndex((z) => z == inputAutocomplete) == -1) {
+      setOptionsCodedgec([...optionsCodedgec, valor]);
+    }
+    setInputCodedgecValue(valor);
+  };
+
   return (
     <Container>
       <Box>
@@ -109,21 +176,62 @@ const UsoInternoDireccionEstudios: React.FC<
         />
       </Box>
 
-      <Typography> INTERNO - Código del Programa (SIGA) </Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-        <FormControl fullWidth>
-          <Select
-            label="INTERNO - Código del Programa (SIGA)"
-            value={departamento}
-            onChange={(e) => setDepartamento(e.target.value as string)}
-            // disabled={readOnly}
-          ></Select>
-        </FormControl>
+      <Box sx={{ marginTop: 4, width: '100%' }}>
+        <Typography>INTERNO - Código Programa (SIGA)</Typography>
+
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Autocomplete
+              value={inputAutocomplete}
+              options={optionsDirest}
+              onChange={handleInputAutoCompleteChange}
+              onKeyDown={handleKeyPress}
+              renderInput={(params) => (
+                <TextField onChange={handleInputDirestChange} {...params} />
+              )}
+            />
+          </Grid>
+
+          <Grid item xs={3}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              className="float-left"
+              onClick={handleAdd}
+              sx={{ marginTop: 2 }}
+            >
+              Agregar
+            </Button>
+          </Grid>
+        </Grid>
+
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Módulo</TableCell>
+                <TableCell>Editar</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tableData.map((data, index) => (
+                <TableRow key={index}>
+                  <TableCell>{data}</TableCell>
+                  <TableCell>
+                    <Button onClick={() => handleEdit(index)}>Editar</Button>
+                    <Button onClick={() => handleDelete(index)}>
+                      Eliminar
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
 
       <Typography>
-        {' '}
-        INTERNO - Periodo académico en que se impartirá (SIGA){' '}
+        INTERNO - Periodo académico en que se impartirá (SIGA)
       </Typography>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
         <FormControl fullWidth>
@@ -131,7 +239,6 @@ const UsoInternoDireccionEstudios: React.FC<
             label="INTERNO - Periodo académico en que se impartirá (SIGA)"
             value={departamento}
             onChange={(e) => setDepartamento(e.target.value as string)}
-            // disabled={readOnly}
           >
             <MenuItem key="Tiempo1">2024- 1</MenuItem>
             <MenuItem key="Tiempo2">2023- 2</MenuItem>
@@ -141,18 +248,10 @@ const UsoInternoDireccionEstudios: React.FC<
       </Box>
 
       <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-        <Button
-          variant="outlined"
-          onClick={onGuardar}
-          //  disabled={readOnly}
-        >
+        <Button variant="outlined" onClick={onGuardar}>
           Guardar
         </Button>
-        <Button
-          variant="outlined"
-          onClick={onEnviar}
-          // disabled={readOnly}
-        >
+        <Button variant="outlined" onClick={onEnviar}>
           Enviar
         </Button>
       </Box>
